2027a33bec4c6151b8856b7cd1e17096eaaeb1aa
kenjiru <kenjiru.ro@gmail.com>
2016-07-13 17:16:45 +0200

    Forgot to export the new helper function.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@
 
 export { promiseTrackMiddleware } from "./middleware";
 export { promiseTrackReducer } from "./reducer";
-export { getLoadingState } from "./helpers";
+export { getLoadingState, getItemLoadingState } from "./helpers";
