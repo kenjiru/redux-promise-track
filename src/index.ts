01227c60d1bef1ba93161e2e58fa55c52365d439
kenjiru <kenjiru.ro@gmail.com>
2016-07-13 15:21:48 +0200

    Fixed wrong import.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@
 
 export { promiseTrackMiddleware } from "./middleware";
 export { promiseTrackReducer } from "./reducer";
-export { getLoadingState } from "./getLoadingState";
+export { getLoadingState } from "./helpers";
