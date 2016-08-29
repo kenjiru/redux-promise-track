affa065aa21507e709415f8dae61b99b14b0db9f
kenjiru <kenjiru.ro@gmail.com>
2016-08-29 14:49:16 +0200

    Upgraded to typescript@2.0 and specified explicitly the lib features.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-///<reference path="../node_modules/typescript/lib/lib.es6.d.ts"/>
-
 export { promiseTrackMiddleware } from "./middleware";
 export { promiseTrackReducer } from "./reducer";
 export { getLoadingState, getItemLoadingState } from "./helpers";
